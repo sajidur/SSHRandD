f1266da68b83f326ddcebebbd076bd2c9446d497
sajidur <sajidur@asabd.org>
2022-11-13 15:50:50 +0600

    first alll

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SSHRandD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSSH = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSSH
+            // 
+            this.btnSSH.Location = new System.Drawing.Point(368, 161);
+            this.btnSSH.Name = "btnSSH";
+            this.btnSSH.Size = new System.Drawing.Size(75, 23);
+            this.btnSSH.TabIndex = 0;
+            this.btnSSH.Text = "SSH";
+            this.btnSSH.UseVisualStyleBackColor = true;
+            this.btnSSH.Click += new System.EventHandler(this.btnSSH_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSSH);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnSSH;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+using Renci.SshNet;
+using System.Diagnostics;
+using System.Text;
+
+namespace SSHRandD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSSH_Click(object sender, EventArgs e)
+        {
+            RunCommand();
+        }
+        private void RunCommand()
+        {
+            var host = "192.168.240.11";
+            var username = "sajidur";
+            var password = "Asa@12345";
+
+            byte[] expectedFingerPrint = new byte[] {
+                                            0x66, 0x31, 0xaf, 0x00, 0x54, 0xb9, 0x87, 0x31,
+                                            0xff, 0x58, 0x1c, 0x31, 0xb1, 0xa2, 0x4c, 0x6b
+                                        };
+
+            using (var client = new SshClient(host, username, password))
+            {
+                client.HostKeyReceived += (sender, e) =>
+                {
+                    if (expectedFingerPrint.Length == e.FingerPrint.Length)
+                    {
+                        //for (var i = 0; i < expectedFingerPrint.Length; i++)
+                        //{
+                        //    if (expectedFingerPrint[i] != e.FingerPrint[i])
+                        //    {
+                        //        e.CanTrust = false;
+                        //        break;
+                        //    }
+                        //}
+                        e.CanTrust = true;
+                    }
+                    else
+                    {
+                        e.CanTrust = false;
+                    }
+                };
+                client.Connect();
+                var sshcommand= client.CreateCommand("ls");
+                var result=sshcommand.Execute();
+            }
+        }
+    }
+}
